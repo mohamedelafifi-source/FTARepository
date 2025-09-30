e2c6da55e838bc0ffe8f157fef443342ed84d399
MohamedElAfifi <mohamed.elafifi@gmail.com>
2025-09-30 08:27:47 -0700

    Initial Commit

---
+++ MyFamilyTree/MyFamilyTreeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFamilyTreeApp.swift
+//  MyFamilyTree
+//
+//  Created by Mohamed El Afifi on 9/30/25.
+//
+
+import SwiftUI
+
+@main
+struct MyFamilyTreeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MyFamilyTreeTests/MyFamilyTreeTests.swift
@@ -0,0 +1,17 @@
+//
+//  MyFamilyTreeTests.swift
+//  MyFamilyTreeTests
+//
+//  Created by Mohamed El Afifi on 9/30/25.
+//
+
+import Testing
+@testable import MyFamilyTree
+
+struct MyFamilyTreeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
